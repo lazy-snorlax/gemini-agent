d4a1b900b7bc2f904e02ca1bfa46775121b424c7
Anthony <blacknebula236@gmail.com>
2025-06-30 20:50:39 +0930

    set up user input

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
+import sys
 import os
 from dotenv import load_dotenv
 from google import genai
 
+def main():
+    load_dotenv()
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+    args = sys.argv[1:]
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
 
-print(response.text + "\n")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count} \n")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count} \n")
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=user_prompt,
+    )
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count} \n")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count} \n")
+    print("Response: ")
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
 
