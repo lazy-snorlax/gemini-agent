102da08f0c7a0db0fad7f5b57808aefec9c75dc4
Anthony <blacknebula236@gmail.com>
2025-06-30 20:58:37 +0930

    store prompt history in message list

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 import os
-from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+
 
 def main():
     load_dotenv()
@@ -18,15 +20,22 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+    generate_content(client, messages)
+
+def generate_content(client, messages):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
-        contents=user_prompt,
+        contents=messages,
     )
 
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count} \n")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count} \n")
     print("Response: ")
     print(response.text)
+    
 
 if __name__ == "__main__":
     main()
