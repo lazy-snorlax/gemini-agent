df346fe8be052222e1310b602a6c59e11e64901e
Anthony <blacknebula236@gmail.com>
2025-07-12 22:23:58 +0930

    create a function to write content to files

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,3 +1,24 @@
+import os
 
 def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_file = abs_working_dir
+
+    if file_path:
+        target_file = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not target_file.startswith(abs_working_dir):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    
+    try:
+        if not os.path.exists(os.path.dirname(target_file)):
+            os.makedirs(os.path.dirname(target_file))
+        
+        with open(target_file, "w") as f:
+            f.write(content)
+        
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Error: {e}"
+    
     return
