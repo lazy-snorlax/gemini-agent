b477f0bd88d8befc6775972e97b3464933db9291
Anthony <blacknebula236@gmail.com>
2025-07-13 22:05:24 +0930

    remove dead return

--- functions/write_file.py
+++ functions/write_file.py
@@ -20,5 +20,3 @@ def write_file(working_directory, file_path, content):
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
         return f"Error: {e}"
-    
-    return
