f4f8c483718c87750507ba41affe0768ca321937
Anthony <blacknebula236@gmail.com>
2025-07-14 22:55:48 +0930

    system prompt + function call output

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 
+from functions.get_files_info import schema_get_files_info
+from call_function import available_functions
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 def main():
     load_dotenv()
@@ -35,12 +47,19 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
         contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
     if verbose:
         print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
         print("Response tokens: ", response.usage_metadata.candidates_token_count)
-    print("Response: ")
-    print(response.text)
+    
+    if not response.function_calls:
+        return response.text
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
     
 
 if __name__ == "__main__":
