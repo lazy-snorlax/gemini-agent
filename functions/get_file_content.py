be7d4725aae165e6210db757b3a1038c770511c4
Anthony <blacknebula236@gmail.com>
2025-07-14 23:06:43 +0930

    add more LLM declarations

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 MAX_CHARS = 10000
 
@@ -20,4 +21,19 @@ def get_file_content(working_directory, file_path):
             return f'{file_content_string} [...File "{target_file}" truncated at {MAX_CHARS} characters]'
     except Exception as e:
         return f"Error: {e}"
-    
+
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+                "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
