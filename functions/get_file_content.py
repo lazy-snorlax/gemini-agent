7954dec2d8478617ada7397fac85a951f00bcd31
Anthony <blacknebula236@gmail.com>
2025-07-15 13:35:32 +0930

    add config file

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,7 +1,6 @@
 import os
 from google.genai import types
-
-MAX_CHARS = 10000
+from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
